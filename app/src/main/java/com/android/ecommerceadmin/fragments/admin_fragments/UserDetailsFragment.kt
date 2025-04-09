c01e67c8b1b96a10aa8cb574789b24aa37f61d3b
khaled <khmeltazy@gmail.com>
2025-04-10 00:48:22 +0200

    implementing fragment_user_details.xml

---
+++ app/src/main/java/com/android/ecommerceadmin/fragments/admin_fragments/UserDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.android.ecommerceadmin.fragments.admin_fragments
+
+import androidx.fragment.app.Fragment
+import com.android.ecommerceadmin.databinding.FragmentUserDetailsBinding
+
+class UserDetailsFragment : Fragment() {
+    private lateinit var binding : FragmentUserDetailsBinding
+}
